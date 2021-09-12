ddbe771ce03522b0e597e77e9e7c56d0c77ebec4
Nayemul <nayemulhasan2016@gmail.com>
2021-09-12 18:08:29 +0600

    update state

--- src/components/AddToBook/AddToSaveList.jsx
+++ src/components/AddToBook/AddToSaveList.jsx
@@ -3,12 +3,14 @@ import fakeData from "../../fakeData"
 import BookFavorite from './BookFavorite';
 import BookList from './BookList';
 const AddToSaveList = () => {
-    console.log(fakeData);
-    const [addToCart, setAddToCart] = useState({
-        data: fakeData,
-        isloading: false
-    });
+    // console.log(fakeData);
+    const [addToCart, setAddToCart] = useState({data: fakeData});
     const [booksFav, setBooksFav] = useState([]);
+    const [addBook, setAddBook] = useState({
+      name: "",
+      description: "",
+      author: ""
+    })
     const addToFavorite = id => {
       const data =addToCart.data.find(item => item.id === id);
       setBooksFav([...booksFav, data]);
@@ -16,17 +18,63 @@ const AddToSaveList = () => {
     const deleteToFavorite = id => {
       const remaining = booksFav.filter(item => item.id !== id);
       setBooksFav(remaining);
-      console.log("after remove",remaining)
+      // console.log("after remove",remaining)
     };
-    console.log("favorite", booksFav);
+    // console.log("favorite", booksFav);
+
+    const addValue =(e)=>{
+      let bookData = {...addBook}
+      bookData[e.target.name] = e.target.value
+      console.log(bookData);
+      setAddBook(bookData);
+    }
+    const addNewBook =(e)=>{
+      e.preventDefault();
+      // const newData = {
+      //   id: new Date().getTime(),
+      //   name:addBook.name,
+      //   description: addBook.description,
+      //   author: addBook.author
+      // }
+  
+      // console.log(newData)
+    }
     return (
         <div className="">
             <div className="container">
-            <h1 className="center">Exercise : Add Favorite Books</h1>
-            {/* <input /> */}
+              <h1 className="center">Exercise : Add Favorite Books</h1>
+              <div className="row">
+                <form onSubmit={addNewBook} className="col-md-4">
+                  <input 
+                    type="text" 
+                    name="name" 
+                    placeholder="name" 
+                    value = {addBook.name}
+                    // onChange={(e)=>setAddBook(e.target.value)}
+                    onChange={addValue}
+                  />
+                
+                  <textarea
+                    type="text"
+                    name="description"
+                    value = {addBook.description}
+                    // onChange={(e)=>setAddBook(e.target.value)}
+                    onChange={addValue}
+                  />
+                  <input
+                    type="text"
+                    name="author"
+                    placeholder="author"
+                    value= {addBook.author}
+                    // onChange={(e)=>setAddBook(e.target.value)}
+                    onChange={addValue}
+                  />
+                  <button>add book</button>
+                </form>
+              </div>
             <div className="row">
               <div className="col-md-6">
-                <BookList books={addToCart.data} add ={addToFavorite}  />
+                <BookList books={addToCart.data} add ={addToFavorite} addB= {addBook} />
                 <hr />
               </div>
               <div className="col-md-5">
@@ -34,9 +82,6 @@ const AddToSaveList = () => {
                   favLists={booksFav}
                   remove={deleteToFavorite}
                 />
-              </div>
-              <div>
-                <button load={addToCart.isloading} />
               </div>
             </div>
           </div>
--- src/components/AddToBook/BookList.jsx
+++ src/components/AddToBook/BookList.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './BookList.css'
-const BookList = ({books,add}) => {
+const BookList = ({books,add, addB}) => {
     return (
+        <>{
         books.map((book) =>{
             return(<div className="bookCard">
                 <h4>{book.name}</h4>
@@ -10,7 +11,16 @@ const BookList = ({books,add}) => {
                 <button onClick={() => add(book.id)}>Add to Fav</button>
             </div>
             )
-        })
+        })}
+        {
+            Object.keys(addB).map(function(key, index) {
+                <div>
+                    <p>{addB.name}</p>
+                </div>
+              })
+              
+        }
+        </>
     )
 }
 
